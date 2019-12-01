daf0e362f8d637e0a85b5c5eab103d18343d1009
Justin Donayre <jdonay3@uic.edu>
2019-11-30 20:19:51 -0600

    New changes, with a game over

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,11 +7,15 @@ public class PlayerScript : MonoBehaviour
 {
   public int numLives;
   PlayerScript m;
+  public GameObject audioSource;
+  public AudioSource gameOver;
   // Start is called before the first frame update
   void Start()
   {
     numLives = 3;
     m = GameObject.Find("HeadAnchor").GetComponent<PlayerScript>();
+    audioSource = GameObject.Find("gameover");
+    gameOver = audioSource.GetComponent<AudioSource>();
   }
 
   // Update is called once per frame
@@ -38,11 +42,24 @@ public class PlayerScript : MonoBehaviour
   public void LoseLife()
   {
     numLives--;
-    if(numLives == 0)
-    {
-      restartScene();
-    }
+        if (numLives == 0)
+        {
+            if (!gameOver.isPlaying)
+            {
+                gameOver.Play();
+                StartCoroutine(GameOverSound());
+
+            }
+        }
+    
   }
+
+    IEnumerator GameOverSound()
+    {
+        yield return new WaitForSeconds(gameOver.clip.length);
+        restartScene();
+    }
+
   public int GetNumLives()
   {
     return numLives;
